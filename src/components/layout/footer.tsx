a6a5abcbe348f7a4d7a6fb166db651baa68fbdac
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-10-10 17:59:17 +0500

    commit

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,9 +2,9 @@ import BgTexture from "../shared/bg-texture";
 
 const Footer = () => {
   return (
-    <footer className="relative shadow-top w-[1920px] text-center mx-auto h-[123px] py-6">
+    <footer className="relative shadow-top w-[1920px] object-center text-center mx-auto h-[123px] py-6">
       <BgTexture className='bg-[url("/images/footer-shape.svg")] footer-path' />
-      <div className="container top-0 right-0 left-0 bottom-0 relative flex flex-col gap-4">
+      <div className="container top-0 left-0 bottom-0 relative flex flex-col gap-4">
         <div className="flex items-center justify-between justify-items-center">
           <div className="kaushan">Ezizgeldi Helleňow</div>
           <div className="text-14 !leading-[140%]">
--- src/pages/poems.tsx
+++ src/pages/poems.tsx
@@ -1,109 +1,108 @@
-import PageLayout from '@/components/layout/page-layout';
-import Pagination from '@/components/shared/pagination';
-import PoemsItem from '@/components/shared/poems-item';
-import SortModal from '@/components/shared/sort-modal';
-import { cn } from '@/lib/utils';
-import { ActiveSort } from '@/store/useSynlar';
-import { useEffect, useState } from 'react';
+import PageLayout from "@/components/layout/page-layout";
+import Pagination from "@/components/shared/pagination";
+import PoemsItem from "@/components/shared/poems-item";
+import SortModal from "@/components/shared/sort-modal";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const data = [
   {
-    name: 'Ýaşyl Tugly Türkmenistan',
+    name: "Ýaşyl Tugly Türkmenistan",
   },
   {
-    name: 'Gel, kalbym, Gel, gürleşeli!',
+    name: "Gel, kalbym, Gel, gürleşeli!",
   },
   {
-    name: 'Ýürek bilen sorag-jogap',
+    name: "Ýürek bilen sorag-jogap",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Ýaşyl Tugly Türkmenistan',
+    name: "Ýaşyl Tugly Türkmenistan",
   },
   {
-    name: 'Gel, kalbym, Gel, gürleşeli!',
+    name: "Gel, kalbym, Gel, gürleşeli!",
   },
   {
-    name: 'Ýürek bilen sorag-jogap',
+    name: "Ýürek bilen sorag-jogap",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Ýaşyl Tugly Türkmenistan',
+    name: "Ýaşyl Tugly Türkmenistan",
   },
   {
-    name: 'Gel, kalbym, Gel, gürleşeli!',
+    name: "Gel, kalbym, Gel, gürleşeli!",
   },
   {
-    name: 'Ýürek bilen sorag-jogap',
+    name: "Ýürek bilen sorag-jogap",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Ýaşyl Tugly Türkmenistan',
+    name: "Ýaşyl Tugly Türkmenistan",
   },
   {
-    name: 'Gel, kalbym, Gel, gürleşeli!',
+    name: "Gel, kalbym, Gel, gürleşeli!",
   },
   {
-    name: 'Ýürek bilen sorag-jogap',
+    name: "Ýürek bilen sorag-jogap",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Ýaşyl Tugly Türkmenistan',
+    name: "Ýaşyl Tugly Türkmenistan",
   },
   {
-    name: 'Gel, kalbym, Gel, gürleşeli!',
+    name: "Gel, kalbym, Gel, gürleşeli!",
   },
   {
-    name: 'Ýürek bilen sorag-jogap',
+    name: "Ýürek bilen sorag-jogap",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Ýaşyl Tugly Türkmenistan',
+    name: "Ýaşyl Tugly Türkmenistan",
   },
   {
-    name: 'Gel, kalbym, Gel, gürleşeli!',
+    name: "Gel, kalbym, Gel, gürleşeli!",
   },
   {
-    name: 'Ýürek bilen sorag-jogap',
+    name: "Ýürek bilen sorag-jogap",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
   {
-    name: 'Gyşdan soňra bahar gelýär!',
+    name: "Gyşdan soňra bahar gelýär!",
   },
 ];
 
 const Poems = () => {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [sort, setSort] = useState({
-    id: 'new',
-    view: 'Snaçala nowye',
+    id: "new",
+    view: "Snaçala nowye",
   });
 
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -114,32 +113,44 @@ const Poems = () => {
   };
 
   const filterData = data.filter((item) =>
-    item.name.toLowerCase().includes(searchValue.toLowerCase()),
+    item.name.toLowerCase().includes(searchValue.toLowerCase())
   );
 
   const totalItems = filterData.length;
-  const displayedData = filterData.slice((currentPage - 1) * perPage, currentPage * perPage);
+  const displayedData = filterData.slice(
+    (currentPage - 1) * perPage,
+    currentPage * perPage
+  );
 
   return (
     <PageLayout
       title="Täze goşgular"
       text="Dobro pozhalovat' v razdel «Synlar, ýatlamar, gutlaglar» nashego saita, gde kazhdoe slovo napolneno iskrennost'yu i teplotoy. Zdes' vy naydete utonchennye stikhi i prozu, kotorye pokoryat serdtsa vashikh blizkikh i druzey svoey krasotoy i glubinoy emotsiy."
-      className="gap-[64px]">
-      <SortModal search={searchValue} setSearch={setSearchValue} sort={sort} setSort={setSort} />
+      className="gap-[64px]"
+    >
+      <SortModal
+        search={searchValue}
+        setSearch={setSearchValue}
+        sort={sort}
+        setSort={setSort}
+      />
 
       <div className="flex flex-col gap-8 relative">
         {searchValue.length > 0 && filterData && (
           <div
             className={cn(
-              'w-full mx-auto  text-center pb-4 h-[50px]',
-              filterData.length !== 0 && 'border-b border-OUTLINE',
-            )}>
-            {filterData.length > 0 ? `Po «${searchValue}» zaprosy naýdeno:` : 'Niçego ne naýdeno!'}
+              "w-full mx-auto  text-center pb-4 h-[50px]",
+              filterData.length !== 0 && "border-b border-OUTLINE"
+            )}
+          >
+            {filterData.length > 0
+              ? `Po «${searchValue}» zaprosy naýdeno:`
+              : "Niçego ne naýdeno!"}
           </div>
         )}
         <div className="grid grid-cols-2 gap-8 w-[948px] mx-auto">
           {displayedData.map((item, i) => (
-            <PoemsItem key={i} id={'1'} {...item} />
+            <PoemsItem key={i} id={"1"} {...item} />
           ))}
         </div>
 
