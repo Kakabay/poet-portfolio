47c7f31e458f65378d942fd9be77d1740905ef7a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-13 14:48:27 +0500

    clean code process

---
+++ src/lib/get-sort-data-poems.ts
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+interface ReturnProps {}
+
+export const getSortPoems = () => {
+  const [sort, setSort] = useState({
+    id: 'new',
+    view: 'Snaçala nowye',
+  });
+};
